ed776c01076be2b924c080d08c2d8351b1b5ae0a
JoseDavid2607 <jose-david2000@hotmail.com>
2026-06-09 18:49:28 -0500

    Update Full-time_Faculty_Activities.py

--- dashboards/Faculty/Full-time_Faculty_Activities.py
+++ dashboards/Faculty/Full-time_Faculty_Activities.py
@@ -2,6 +2,7 @@
 #  Full-time Faculty Activities · UASM
 #  Self-contained · no external module dependencies
 # ===========================================================================
+import re
 import streamlit as st
 import pandas as pd
 import numpy as np
