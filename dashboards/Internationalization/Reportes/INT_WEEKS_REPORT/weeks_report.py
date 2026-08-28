849cac478b2cfa6ce8097f487c76540f8f96ca2f
JoseDavid2607 <jose-david2000@hotmail.com>
2026-08-28 11:18:23 -0500

    Change page icon from globe to airplane

--- dashboards/Internationalization/Reportes/INT_WEEKS_REPORT/weeks_report.py
+++ dashboards/Internationalization/Reportes/INT_WEEKS_REPORT/weeks_report.py
@@ -38,7 +38,7 @@ import base64
 # ── 1) CONFIGURACIÓN GLOBAL ────────────────────────────────────────────────
 st.set_page_config(
     page_title="International Weeks Analytics",
-    page_icon="🌍",
+    page_icon="🛫",
     layout="wide",
     initial_sidebar_state="expanded",
 )
